aca4ebc28c15e13426555713ad1f2a2aa6d02902
Dhruv Raikwar <dratarra2007@gmail.com>
2026-09-22 20:23:22 +0530

    fixinng 1 bug

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ async function main() {
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto:{
-    secret:procces.env.SECRET,
+    secret:process.env.SECRET,
   },
   touchAfter: 24*3600,
 })
@@ -62,7 +62,7 @@ store.on("error",()=>{
 //iniliaing the session option for session
 const sessionOption = session({
   store,
-  secret: procces.env.SECRET,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
